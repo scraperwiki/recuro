fbf52f27e664325c3ca6764ed03ce1df3172122e
David Jones <drj@pobox.com>
2014-03-31 15:37:59 +0100

    Force the testing tools to be locally installed
    This worksaround the problem that drj has where nosetests is
    already installed as a "site package".

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ virtualenv --system-site-packages venv
 . venv/bin/activate
 
 pip install -r requirements.txt
-pip install -r dev_requirements.txt
+pip install -I -r dev_requirements.txt
 
 echo "This script only runs non-integration tests"
 export DJANGO_SETTINGS_MODULE=recuro_app.test_settings
