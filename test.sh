0707f43ad29f822883cbf79ca1b3cb163f485146
Tom Mortimer-Jones <tom@morty.co.uk>
2014-03-31 14:43:13 +0100

    Added special character to successful payment XML to test encoding changes made in commit 6549864697fa3ff8231e2d7ffc8f424d7b252ed6

--- test.sh
+++ test.sh
@@ -4,4 +4,4 @@ echo "This script only runs non-integration tests"
 export DJANGO_SETTINGS_MODULE=recuro_app.test_settings
 export PYTHONPATH=.
 
-specloud -a '!integration'
+specloud -s -a '!integration'
