e50e99429bc6a44a8d53f047a7053db7a4ee7d3c
Tom Mortimer-Jones <tom@morty.co.uk>
2014-03-31 14:56:16 +0100

    Added requirements back into this repo

--- test.sh
+++ test.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+virtualenv --system-site-packages venv
+. venv/bin/activate
+
+pip install -r requirements.txt
+pip install -r dev_requirements.txt
+
 echo "This script only runs non-integration tests"
 export DJANGO_SETTINGS_MODULE=recuro_app.test_settings
 export PYTHONPATH=.
